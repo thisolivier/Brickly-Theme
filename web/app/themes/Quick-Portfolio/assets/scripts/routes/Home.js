052957e2f948106e7f0b535309a40d0d307c8a0e
Olivier <olivier@olivier.uk>
2016-11-23 12:27:31 +0000

    Chimney class handles last article correctly. Scripts reformatted

--- web/app/themes/Quick-Portfolio/assets/scripts/routes/Home.js
+++ web/app/themes/Quick-Portfolio/assets/scripts/routes/Home.js
@@ -1,74 +1,94 @@
+// jshint max-len:0
+
 export default {
   init() {
     class Logger {
       static log(printMe) {
-        this.log = !this.log ? 1 : this.log += 1;
+        this.index = !this.index ? 1 : this.index += 1;
+        // const pad = number => (number <= 99 ? (`000${number}`).slice(-2) : number);
+        const indexAPI = this.index;
         let nowTime = new Date();
-        nowTime = `${nowTime.getMinutes()}.${nowTime.getSeconds()}`;
-        const prefix = `## Captain's log, stardate ${this.log}:${nowTime} ->`;
+        nowTime = `${nowTime.getMilliseconds()}`;
+        const prefix = `# Captain's log, stardate ${indexAPI}.${nowTime} ->`;
         if (printMe) {
-          console.log(`${prefix} ${printMe} ##`);
+          console.log(`${prefix} \n## ${printMe}`);
         } else {
-          console.log(`${prefix} Space is vast and empty (-__-) ##`);
+          console.log(`${prefix} \n## Space is vast and empty (-__-)`);
         }
       }
     }
 
-
     class ChimneyStack {
       constructor() {
-        this.index = { brick: 1 };
+        this.index = { brick: 1, articles: 0 };
         this.append = [];
         this.pattern = { open: ['b', 's'], repeat: ['s', 'b'] };
-        this.brickSizes = { b: 'big_brick', s: 'small_brick' };
-        this.$articles = $('main').children('article');
+        this.brickClass = { b: 'big_brick', s: 'small_brick' };
+        this.$articles = $('main').children('article').detach();
       }
-      elementCreator(template) {
-        const newbie = document.createElement('div');
-        newbie.setAttribute('class', this.brickSizes[template]);
-        newbie.setAttribute('aria-hidden', 'true');
-        return newbie;
+
+      conductor() {
+        this.usePattern('open');
+        let emergencyStop = 0;
+        while (this.append.length < 13) {
+          this.usePattern('repeat');
+          if (emergencyStop > 15) { Logger.log('Emergency stop'); break; }
+          emergencyStop += 1;
+        }
+        $('main').append(this.append);
       }
+
       usePattern(key) {
+        Logger.log(`We are on the ${key} set`);
         for (const current of this.pattern[key]) {
           const newbie = (key === 'open') || (this.$articles.length === 0) ?
-            this.elementCreator(current) : this.factory(current);
+            this.divHollow(current) : this.divBespoke(current);
           this.append.push(newbie);
         }
       }
-      factory(element) {
+
+      divHollow(template) {
+        const newbie = document.createElement('div');
+        newbie.addClass(this.brickClass[template]);
+        newbie.setAttribute('aria-hidden', 'true');
+        return newbie;
+      }
+
+      divBespoke(element) {
+        const virgin = this.index.brick;
         const kiln = {
-          virgin: this.index.brick,
           full: () => {
-            const newbie = this.$articles.eq(0);
+            const newbie = this.$articles.eq(this.index.articles);
             this.index.brick = 0;
-            if (newbie.length === 1) {
-              this.$articles = this.$articles.splice(0, 1);
+            if (newbie.length) {
+              this.index.articles += 1;
+              newbie.addClass(this.brickClass[element]);
               return newbie;
             }
+            this.index.articles = 0;
             return kiln.hollow();
           },
-          hollow: () => this.elementCreator(element),
+          hollow: () => this.divHollow(element),
         };
-        return (((element === 'b') && (Math.random() > 0.3)) || kiln.virgin ?
+
+        return (virgin || ((element === 'b') && (Math.random() > 0.35) && this.index.articles) ?
           kiln.full() : kiln.hollow()
         );
       }
     }
 
     const chim = new ChimneyStack();
-    Logger.log(chim.usePattern('open'));
+    Logger.log(chim.conductor());
 
     /* THE PLAN
-
-YES      create template empty elements, big (Be) and small (Ba)
-YES      find articles, store number
-YES      enable debugging and test articles have been found
-NA      pattern now = BaBaBaBaBa...n(articles)
-YES      Create pattern ref = SeBa,BoSe,SeBo,BoSe
-YES      Create new object, Store BeSe then begin adding elements according to the pattern,
-YES      If element is Bo, .33 probability of becoming Ba,
-YES      If element is Ba, use next article instead of template element
+    YES      create template empty elements, big (Be) and small (Ba)
+    YES      find articles, store number
+    YES      enable debugging and test articles have been found
+    NA      pattern now = BaBaBaBaBa...n(articles)
+    YES      Create pattern ref = SeBa,BoSe,SeBo,BoSe
+    YES      Create new object, Store BeSe then begin adding elements according to the pattern,
+    YES      If element is Bo, .33 probability of becoming Ba,
+    YES      If element is Ba, use next article instead of template element
     */
   },
   finalize() {
