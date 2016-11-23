5ec71dc74183b0cfb8450df1539c10ee9fa32f01
Olivier <olivier@olivier.uk>
2016-11-23 13:30:30 +0000

    Code optimised, notes added

--- web/app/themes/Quick-Portfolio/assets/scripts/routes/Home.js
+++ web/app/themes/Quick-Portfolio/assets/scripts/routes/Home.js
@@ -3,6 +3,9 @@
 export default {
   init() {
     class Logger {
+      // Just a logging script which makes things readable,
+      // tracks the number of entries its making,
+      // and the time my script is taking to run.
       static log(printMe) {
         this.index = !this.index ? 1 : this.index += 1;
         // const pad = number => (number <= 99 ? (`000${number}`).slice(-2) : number);
@@ -17,21 +20,25 @@ export default {
           Logger.print(prefix, 'Space is vast and empty (-__-)');
         }
       }
+      // This is iscolated incase I later change the formatting, or use a logging plugin
       static print(prefix, printMe) {
         console.log(`${prefix} ${printMe}`);
       }
     }
 
     class ChimneyStack {
+      // The script which modified the posts so they can be laid out like a brick wall
       constructor() {
-        this.index = { brick: 1, articles: 0 };
+        this.index = { brick: 1, articles: 1 };
         this.append = [];
         this.pattern = { open: ['b', 's'], repeat: ['s', 'b'] };
         this.brickClass = { b: 'big_brick', s: 'small_brick' };
+        // Finds my articles, stores them and removes them from the DOM
         this.$articles = $('main').children('article').detach();
         Logger.log('begin');
       }
 
+      // Runs through the pattern, ensuring min and max repetitions
       conductor() {
         this.usePattern('open');
         let emergencyStop = 0;
@@ -40,18 +47,21 @@ export default {
           if (emergencyStop > 15) { Logger.log('Emergency stop'); break; }
           emergencyStop += 1;
         }
+        // At last, we inserts the result back into the DOM
         $('main').append(this.append);
       }
 
+      // Is passed a section of the pattern, and works though its items
       usePattern(key) {
         Logger.log(`We are on the ${key} set`);
         for (const current of this.pattern[key]) {
-          const newbie = (key === 'open') || (this.$articles.length === 0) ?
+          const newbie = (key === 'open') || (!this.index.articles) ?
             this.divHollow(current) : this.divBespoke(current);
           this.append.push(newbie);
         }
       }
 
+      // Creates an empty div (brick) with class approprite to the pattern
       divHollow(template) {
         const newbie = document.createElement('div');
         $(newbie).addClass(this.brickClass[template]);
@@ -59,11 +69,13 @@ export default {
         return newbie;
       }
 
+      // Inserts either a hollow brick or an article brick
       divBespoke(element) {
         const virgin = this.index.brick;
+        // The kiln can produce hollow brick, or one with an article.
         const kiln = {
           full: () => {
-            const newbie = this.$articles.eq(this.index.articles);
+            const newbie = this.$articles.eq(this.index.articles - 1);
             this.index.brick = 0;
             if (newbie.length) {
               this.index.articles += 1;
@@ -75,8 +87,10 @@ export default {
           },
           hollow: () => this.divHollow(element),
         };
-
-        return (virgin || ((element === 'b') && (Math.random() > 0.35) && this.index.articles) ?
+        // If this is a chance for an article, have think,
+        // If we're a virgin, or if the odds are good, insert an article.
+        // Failing any of that, make a hollow brick.
+        return ((element === 'b') && (virgin || (Math.random() > 0.35)) ?
           kiln.full() : kiln.hollow()
         );
       }
