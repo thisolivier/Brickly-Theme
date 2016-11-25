86b06ab21def214b59f2d352c65b3fcd1d6d28a5
Olivier <olivier@olivier.uk>
2016-11-25 15:52:16 +0000

    Initial work to brick layout

--- web/app/themes/Quick-Portfolio/templates/partials/content.php
+++ web/app/themes/Quick-Portfolio/templates/partials/content.php
@@ -3,7 +3,9 @@
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('partials/entry-meta'); ?>
   </header>
-  <div class="entry-summary">
-    <?php the_excerpt(); ?>
+  <div class="post-content">
+    <div class="entry-content">
+      <?php the_post(); ?>
+    </div>
   </div>
 </article>
