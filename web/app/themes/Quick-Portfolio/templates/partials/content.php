66807d48af9c336027238b31458b40b179cc5fd4
Olivier <olivier@olivier.uk>
2016-11-25 18:01:36 +0000

    Fixed error in content insertion

--- web/app/themes/Quick-Portfolio/templates/partials/content.php
+++ web/app/themes/Quick-Portfolio/templates/partials/content.php
@@ -3,9 +3,7 @@
     <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
     <?php get_template_part('partials/entry-meta'); ?>
   </header>
-  <div class="post-content">
-    <div class="entry-content">
-      <?php the_post(); ?>
-    </div>
+  <div class="entry-summary">
+    <?php the_content(); ?>
   </div>
 </article>
