bfe56386146c5c0501b452180f4a1f3bf7212a00
Olivier <olivier@olivier.uk>
2017-01-10 21:46:51 +0000

    Post tags now collapse gracefully

--- web/app/themes/Quick-Portfolio/templates/partials/entry-meta.php
+++ web/app/themes/Quick-Portfolio/templates/partials/entry-meta.php
@@ -1,2 +1,6 @@
-<time class="updated" datetime="<?= get_post_time('c', true); ?>"><?= get_the_date(); ?></time>
-<p class="byline tags"><?php the_tags( '','','' ); ?> </p>
+<time class="updated" datetime="<?= get_post_time('c', true); ?>">
+  <?= get_the_date(); ?>
+</time>
+<div class="wrapper">
+  <p class="byline tags"><?php the_tags( '','','' ); ?></p>
+</div>
