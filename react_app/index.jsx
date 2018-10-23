eeaa49a34e2f44b472813f4cfe8d3146ce9a4a50
Olivier Butler <olivier.butler@gmail.com>
2018-10-23 19:26:41 +0100

    It's taling to the wordpress api

--- react_app/index.jsx
+++ react_app/index.jsx
@@ -3,24 +3,53 @@ import { render } from 'react-dom'; // importing render from ReactDOM
 import { Switch, Route, BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+          dataPayload: ""
+        }
+      }
+
+    componentDidMount() {
+        this.setState({
+            dataPayload: "Beans"
+        })
+        let dataURL = "http://olivier.test/wp-json/wp/v2/posts";
+        fetch(dataURL)
+        .then(res => res.json())
+        .then(res => {
+            this.setState({
+                dataPayload: res
+            })
+        })
+    }
+
     render() {
+
+        let payload = this.state.dataPayload
+
         return(
             <div id="page-inner">
                 <h1>Boo</h1>
                 <div id="content">
-                    <Console name="Jim"/>
+                    <Console name="payload"/>
                 </div>
             </div>
         )
     }
+
 }
 
 class Console extends Component {
+
     render() {
         return(
-            <div className="console">I am a console called {this.props.name}</div>
+            <div className="console">I am a console called... {this.props.name}
+            </div>
         )
     }
+
 }
 
 
