65fd878322ff56b1d3c5beec79b6c61e6b0511ce
Olivier Butler <olivier.butler@gmail.com>
2018-10-24 13:15:06 +0100

    Added readme, set core functions to use proper names

---
+++ README.md
@@ -0,0 +1,8 @@
+# Brickly Wordpress
+A React.js Wordpress theme. The theme can be installed as any normal theme can be, and will serve a SPA to your client. It is in the early stages of development.
+
+## Todo
+1. Implement dynamic routing/coorinator flow, see [react-router](https://reacttraining.com/react-router/web/guides/philosophy)
+2. Build interface with Wordpress
+3. Store Wordpress in stores,see [Alt](http://alt.js.org/guide/async/ )
+4. Design basic views
--- functions.php
+++ functions.php
@@ -1,21 +1,21 @@
 <?php
-function celestial_scripts() {
+function brickly_scripts() {
 	// Load our main stylesheet.
 	wp_enqueue_style( 'celestial-style-dist', get_stylesheet_directory_uri() . '/dist/style.css');
 	wp_enqueue_style( 'celestial-style', get_stylesheet_uri() );
 
-    wp_enqueue_script( 'celestial-script', get_stylesheet_directory_uri() . '/react_app_built/app.js' , array(), '1.0', true );
+    wp_enqueue_script( 'react-app', get_stylesheet_directory_uri() . '/react_app_built/app.js' , array(), '1.0', true );
 
     $url = trailingslashit( home_url() );
     $path = trailingslashit( parse_url( $url, PHP_URL_PATH ) );
 
-    wp_scripts()->add_data( 'celestial-script', 'data', sprintf( 'var CelestialSettings = %s;', wp_json_encode( array(
-        'title' => get_bloginfo( 'name', 'display' ),
+    wp_scripts()->add_data( 'react-app', 'data', sprintf( 'var WORDPRESS = %s;', wp_json_encode( array(
+        'site_name' => get_bloginfo( 'name', 'display' ),
         'path' => $path,
-        'URL' => array(
+        'url' => array(
             'api' => esc_url_raw( get_rest_url( null, '/wp/v2' ) ),
             'root' => esc_url_raw( $url ),
         ),
     ) ) ) );
 }
-add_action( 'wp_enqueue_scripts', 'celestial_scripts' );
+add_action( 'wp_enqueue_scripts', 'brickly_scripts' );
