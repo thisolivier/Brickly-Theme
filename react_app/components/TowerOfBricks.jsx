d740bb1f40390d0029db29b49aad543b24d6f0e2
Olivier Butler <olivier.butler@gmail.com>
2018-10-28 10:42:33 +0000

    Made links work

--- react_app/components/TowerOfBricks.jsx
+++ react_app/components/TowerOfBricks.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class TowerOfBricks extends Component {
 
@@ -7,7 +8,7 @@ class TowerOfBricks extends Component {
             <div>
                 {WORDPRESS.category.map((nameDescriptionCountId, index) => 
                 <div className="brick" key={index}>
-                    <h2><a href={"/c/"+nameDescriptionCountId.slug}>{nameDescriptionCountId.name}</a></h2>
+                    <h2><Link to={"/c/"+nameDescriptionCountId.slug}>{nameDescriptionCountId.name}</Link></h2>
                 </div>
                 )}
             </div>
