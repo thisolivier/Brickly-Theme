f8d48be525de95336750a4ea49de2e26252dd00a
Olivier Butler <olivier.butler@gmail.com>
2018-10-29 11:00:09 +0000

    Spotted another placewhere links need updating

--- react_app/components/Footer.jsx
+++ react_app/components/Footer.jsx
@@ -1,15 +1,14 @@
 import React, {Component} from 'react';
+import { Link } from 'react-router-dom';
 
 class Footer extends Component {
     render() {
         return(
             <footer role="complementary">
-            <div className="homeLink">
-                <a href="/" title="Home" className="closePost">
+                <Link to="/" title="Home" className="closePost">
                     <img src="/react_app_built/images/icon-house.png" alt="Home" />
-                </a>
-            </div>            
-        </footer>
+                </Link>       
+            </footer>
         )
     }
 }
