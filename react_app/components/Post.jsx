606aeef6961453e43a447306561b81ad2b00cce3
Olivier Butler <olivier.butler@gmail.com>
2018-11-06 18:57:38 +0000

    Added post tag styling

--- react_app/components/Post.jsx
+++ react_app/components/Post.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
+import Outlinks from './OutLinks';
 
 class Post extends React.Component {
     render() {
         var post = WORDPRESS.posts[this.props.postId]
         return (
-            <div>
-                <img src={post.image} />
-                <h1>{post.title}</h1>
-                <p>{post.date}</p>
-                <p>{post.byLine}</p>
-                <ul>
-                    {post.tags.map(tag=><li>{tag}</li>)}
-                </ul>
-                <p>{post.content}</p>
-                <ul>
+            <div className="post">
+                <h2>{post.title}</h2>
+                <section className="info">
+                    <p>{post.date}</p>
+                    <p>{post.byLine}</p>
+                    <ul className="tags">
+                        {post.tags.map(tag=><li>{tag}</li>)}
+                    </ul>
+                </section>
+                <section>{post.content}</section>
+                <section className="outLinks">
                     <li>{post.repo}</li>
                     <li>{post.liveSite}</li>
-                </ul>
+                    <Outlinks />
+                </section>
+                <img className="backgroundImage" src={post.image} />
             </div>
         )
     }
