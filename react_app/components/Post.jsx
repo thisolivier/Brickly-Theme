6dfe8a0ce23e83f83001bf9ec21ee96343211ec5
Olivier Butler <olivier.butler@gmail.com>
2018-11-07 12:45:29 +0000

    Moved to better supported html parser with fewer dependencies

--- react_app/components/Post.jsx
+++ react_app/components/Post.jsx
@@ -1,30 +1,10 @@
 import React from 'react';
 import Outlinks from './OutLinks';
+import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 
 class Post extends React.Component {
     render() {
         var post = WORDPRESS.posts[this.props.postId]
-        var elements = post.content.split('</section>').map((item,index)=>{
-            console.log('item is', item)
-            var sectionText = item.replace('<section>','').replace(/(\r\n\t|\n|\r\t)/gm,"")
-            var unsafeTitle = sectionText.match(/<h2>(.*?)<\/h2>/g)
-            if (!unsafeTitle){
-                return null
-            }
-            var safeTitle = unsafeTitle[0].replace("<h2>","").replace("</h2>","")
-            console.log("title is", safeTitle)
-            return {
-                title: safeTitle,
-                content: sectionText.replace(unsafeTitle[0],'').split('</p>').map((paragraph, paragraphIndex)=>{
-                    var paragraphString = paragraph.replace('<p>','')
-                    return paragraphString.split(/<a\s|<\/a>/g)
-                })
-            }
-        })
-            
-            
-        
-        console.log(elements)
         return (
             <div className="post">
                 <h2>{post.title}</h2>
@@ -35,7 +15,9 @@ class Post extends React.Component {
                         {post.tags.map((tag, index)=><li key={index}>{tag}</li>)}
                     </ul>
                 </section>
-                <section>{post.content}</section>
+                <section>
+                    {ReactHtmlParser(post.content)}
+                </section>
                 <section className="outLinks">
                     <li>{post.repo}</li>
                     <li>{post.liveSite}</li>
