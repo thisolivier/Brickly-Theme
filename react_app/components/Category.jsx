a7f9e6159abee6a204e33b8c608614a8aaf349ac
Olivier Butler <olivier.butler@gmail.com>
2018-11-29 19:36:38 +0000

    Category page comes in better now, especially the cloud

--- react_app/components/Category.jsx
+++ react_app/components/Category.jsx
@@ -9,8 +9,10 @@ class Category extends React.Component {
         var categoriesPosts = Object.keys(WORDPRESS.posts).filter(postId => WORDPRESS.posts[postId.toString()].categories.includes(categorySlug))
         return (
             <div className="categoryContainer">
-                <h1>{currentCategory.name}</h1>
-                <p className="categoryDescription">{currentCategory.description}</p>
+                <div className="categoryHeaderContainer">
+                    <h1>{currentCategory.name}</h1>
+                    <p className="categoryDescription">{currentCategory.description}</p>
+                </div>
                 <div className="postsContainer">
                     {categoriesPosts.map((postId)=>
                         <Post postId={postId} key={postId}/>
