678f4431130532dc43bf556da2ab5efbdd5cde13
Olivier Butler <olivier.butler@gmail.com>
2018-12-11 12:05:38 +0000

    Posts looking good at regular widths

--- react_app/components/Post.jsx
+++ react_app/components/Post.jsx
@@ -17,31 +17,34 @@ class Post extends React.Component {
 
     render() {
         var readMoreHandler = ()=>{this.setState({showCopy: !this.state.showCopy})}
+        var readMoreText = this.state.showCopy ? "Close ↑": "Read More ↓"
         return (
             <div className="singlePostContainer">
                 <div class="imageWrapperForGradientFallback">
                     <img className="image" src={this.post.image ? this.post.image : ""}></img>
                 </div>
                 <div className="singlePostContent">
-                    <a className="readMoreButton" onClick={readMoreHandler}>Read More ↓</a>
+                    <a className="readMoreButton footerElement" onClick={readMoreHandler}>{readMoreText}</a>
                     <section className="info">
-                        <h2>{this.post.title}</h2>
-                        <p className="byLine">{this.byline}</p>
-                        <ul className="tags">
-                            {this.post.tags.map((tag, index)=><li key={index}>{tag}</li>)}
-                        </ul>
-                        <CSSTransition in={this.state.showCopy} mountOnEnter unmountOnExit timeout={300} classNames="toggle">
-                            <section className="postDescription">
-                                {this.content}
-                            </section>
-                        </CSSTransition>
+                        <div className="widthController">
+                            <h2 className="postTitle">{this.post.title}</h2>
+                            <p className="byLine">{this.byline}</p>
+                            <ul className="tags">
+                                {this.post.tags.map((tag, index)=><li key={index}>{tag}</li>)}
+                            </ul>
+                            <CSSTransition in={this.state.showCopy} mountOnEnter unmountOnExit timeout={300} classNames="toggle">
+                                <section className="postDescription">
+                                    {this.content}
+                                </section>
+                            </CSSTransition>
+                        </div>
                     </section>
-                    <nav className="postLinks">
+                    <nav className="postLinks footerElement">
                         <a href={this.post.liveSite}>Live Site ↗</a>
                         <a href={this.post.repo}>Repository ↗</a>
                     </nav>
                 </div>
-                <div className="decorativeFooter">Boop</div>
+                <div className="decorativeFooter"></div>
             </div>
         )
     }
