4793dd647093d09345813d79e652ba3470d00272
Olivier Butler <olivier.butler@gmail.com>
2019-02-08 12:13:22 +0000

    Removed redundant dependency

--- react_app/components/App.jsx
+++ react_app/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 import {animateScroll as scroll} from 'react-scroll'
 
